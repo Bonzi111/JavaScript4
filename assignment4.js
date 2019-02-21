a781f5f06a8545722ea5a8f30e322d3fc2208684
Shantanu <42547471+Bonzi111@users.noreply.github.com>
2019-02-21 13:14:19 +0530

    Update assignment4.js

--- assignment4.js
+++ assignment4.js
@@ -9,9 +9,10 @@ function showTime()
     var year = d.getFullYear();
     var day= d.getDay();
     var day_name=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var watch= date + ":" + month + ":" + year + ": "+ day_name[day];
+    var month_name=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var watch= date + ":" + month_name[month] + ":" + year + ": "+ day_name[day];
     console.log(watch);
-    var time = hour + ":" + month + ":" + second + " ";
+    var time = hour + ":" + minutes + ":" + second + " ";
     console.log(time);
     setTimeout(showTime, 1000);
 }
